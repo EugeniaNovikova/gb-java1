7e69cb361333a191733e392132af18810a1f87d0
EvgeniaNovikova <eugenia-g@mail.ru>
2021-04-13 11:09:38 +0400

    cicles

---
+++ lesson/src/main/java/Training/cicles.java
@@ -0,0 +1,11 @@
+package Training;
+
+public class cicles {
+    public static void main(String[] args) {
+       int x;
+       for (x = 0; x < 50; x += 5) {
+           System.out.println(x + " ");
+       }
+        System.out.println("end");
+    }
+}
---
+++ lesson/src/main/java/Training/methods.java
@@ -0,0 +1,12 @@
+package Training;
+
+public class methods {
+    public static void main(String[] args) {
+       int a = 6;
+       int b = 6;
+       int c = a + b;
+        System.out.println(" Значение c = " + c);
+    }
+
+    }
+
