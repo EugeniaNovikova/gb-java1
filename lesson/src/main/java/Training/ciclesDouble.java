cc4d4c156ec09e696073b7fe26e31a4b96cb281d
EvgeniaNovikova <eugenia-g@mail.ru>
2021-04-13 12:02:12 +0400

    Вложенные циклы

---
+++ lesson/src/main/java/Training/ciclesDouble.java
@@ -0,0 +1,9 @@
+package Training;
+
+public class ciclesDouble {
+    public static void main(String[] args) {
+        for (int i = 0, j = 10; i < j; i++, j--) {
+            System.out.println("i - j = " + i + " - " + j);
+        }
+    }
+}
