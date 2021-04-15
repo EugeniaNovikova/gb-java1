e31eb0b78878d98f1029b1ad3e418e9ab1132ee7
EvgeniaNovikova <eugenia-g@mail.ru>
2021-04-15 10:47:29 +0400

    Вычисление расстояния до крупного объекта по эху

---
+++ lesson/src/main/java/shildt/Echo.java
@@ -0,0 +1,9 @@
+package shildt;
+
+public class Echo {
+    public static void main(String[] args) {
+        double dist;
+        dist = 330 * 14.3 / 2;
+        System.out.println("Расстояние до крупного объекта равно " + dist + " км");
+    }
+}
