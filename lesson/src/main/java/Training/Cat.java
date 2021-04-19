b7286d869f203ba7b5d8b02f86cf3bb066d8962b
EvgeniaNovikova <eugenia-g@mail.ru>
2021-04-19 15:52:15 +0400

    СОздаем класс котов и задаем им параметры возраста (невозможность установить отрицательный возраст)

--- lesson/src/main/java/Training/Cat.java
+++ lesson/src/main/java/Training/Cat.java
@@ -4,4 +4,15 @@ public class Cat {
     String name;
     String colour;
     int age;
+
+    public void setAge (int Age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            System.out.println("Введен некорректный возраст");
+        }
+    }
+    public int getAge {
+        return age;
+    }
 }
